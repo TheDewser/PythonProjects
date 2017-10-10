277d268de1694107157a15c1ef6e6ab293ab1af0
Mike D <mdews@dewser.com>
2017-10-09 21:03:57 -0400

    works against valid urls

--- urlChecker.py
+++ urlChecker.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python
 import httplib
 
 with open("urls-to-check.txt", "r") as f:
+    urls = f.read().splitlines()
     #urlList2 = []
-    for url in f:
-        c = httplib.HTTPConnection(str(url))
+    for url in urls:
+        c = httplib.HTTPConnection(url)
         c.request("HEAD", '')
         if c.getresponse().status == 200:
-            print('Web site exists')
+            print(url+' - ' +'Web site exists')
         else:
             print('Web site does not exist')
