e1768923c13a957d31c45978c7b5cdcddfc30c74
Mike D <mdews@dewser.com>
2017-10-10 21:44:35 -0400

    added exception handling, list processes as expected.  May need to evaluate performance.

--- urlChecker.py
+++ urlChecker.py
@@ -4,9 +4,12 @@ import httplib
 with open("urls-to-check.txt", "r") as f:
     urls = f.read().splitlines()
     for url in urls:
-        c = httplib.HTTPConnection(url)
-        c.request("HEAD", '')
-        if c.getresponse().status == 200:
-            print(url+' - ' +'Web site exists')
-        else:
-            print('Web site does not exist')
+        try:
+            c = httplib.HTTPConnection(url)
+            c.request("HEAD", '')
+            if c.getresponse().status == 200:
+                print(url+' - ' +'Web site exists')
+            #else:
+            #    print('Web site does not exist')
+        except:
+            print(url+' - '+'Site not available')
